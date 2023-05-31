5951392a754fddcece4ce904d216b1a913c20568
Harilegawa <120030681+WenaHarle@users.noreply.github.com>
2023-05-31 20:39:20 +0700

    Update Game.c

--- Game.c
+++ Game.c
@@ -8,305 +8,229 @@
 #define H 75
 #define N 100
 
-#define V 21
-#define H 75
-#define N 100
-
-/*
-	@author: Alfredo Flores
-	@email: alfredojose20001@outlook.es
-
-*/
+typedef struct
+{
+    int x, y; // initial position
+    int movX, movY;
+    char image;
+} Snake;
 
 typedef struct
 {
+    int x, y;
+} Fruit;
 
-	int x, y; // initial position
-	int movX, movY;
+Snake snake[N]; // maximum size
+Fruit fruit;
 
-	char imagen;
+void initialize(int *size, char map[V][H]);
+void displayIntro(char map[V][H]);
+void setIntroData(char map[V][H], int *size);
+void gameLoop(char map[V][H], int size);
+void getUserInput(char map[V][H], int *size, int *isDead);
+void updateGame(char map[V][H], int size);
+void setIntroData2(char map[V][H], int size);
 
-} snake;
+void gotoxy(int x, int y);
 
-typedef struct
-{
-
-	int x, y;
-
-} fruit;
-
-snake snk[N]; // maximun size
-fruit frt;
-
-void begin(int *size, char map[V][H]);
-void intro(char map[V][H]);
-void intro_data(char map[V][H], int *size);
-void loop(char map[V][H], int size);
-void input(char map[V][H], int *size, int *dead);
-void update(char map[V][H], int size);
-void intro_data2(char map[V][H], int size);
-
-void gotoxy(int x, int y); // This function was suggested by a friend to get better performance
-
-void show(char map[V][H]);
+void showMap(char map[V][H]);
 
 int main()
 {
+    int size;
+    char map[V][H];
 
-	int size;
+    initialize(&size, map);
+    showMap(map);
+    system("pause");
+    gameLoop(map, size);
 
-	char map[V][H];
-
-	begin(&size, map);
-	show(map);
-	system("pause");
-	loop(map, size);
-
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
 
-// Initialized in a initial position
-void begin(int *size, char map[V][H])
+void initialize(int *size, char map[V][H])
 {
-	int i;
-	// snake head
-	snk[0].x = 32;
-	snk[0].y = 10; // V
-	// snake body
-	*size = 4;
+    int i;
 
-	srand(time(NULL));
+    // Set snake head initial position
+    snake[0].x = 32;
+    snake[0].y = 10;
+    *size = 4;
 
-	frt.x = rand() % (H - 2) + 1; // automatic position of fruit
-	frt.y = rand() % (V - 2) + 1;
+    srand(time(NULL));
 
-	for (i = 0; i < *size; i++)
-	{
+    fruit.x = rand() % (H - 2) + 1;
+    fruit.y = rand() % (V - 2) + 1;
 
-		snk[i].movX = 1;
-		snk[i].movY = 0;
+    for (i = 0; i < *size; i++)
+    {
+        snake[i].movX = 1;
+        snake[i].movY = 0;
+    }
 
-	} // snake only move on X
-
-	intro(map);
-	intro_data(map, size);
+    displayIntro(map);
+    setIntroData(map, size);
 }
 
-// Show everything
-void show(char map[V][H])
+void showMap(char map[V][H])
 {
+    int i, j;
 
-	int i, j;
-
-	for (i = 0; i < V; i++)
-	{
-
-		for (j = 0; j < H; j++)
-		{
-
-			printf("%c", map[i][j]);
-		}
-		printf("\n");
-	}
+    for (i = 0; i < V; i++)
+    {
+        for (j = 0; j < H; j++)
+        {
+            printf("%c", map[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-// The camp (map).
-void intro(char map[V][H])
+void displayIntro(char map[V][H])
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i < V; i++)
-	{
-
-		for (j = 0; j < H; j++)
-		{
-
-			if (i == 0 || i == V - 1)
-			{
-
-				map[i][j] = '-';
-			}
-			else if (j == 0 || j == H - 1)
-			{
-
-				map[i][j] = '|';
-			}
-			else
-			{
-				map[i][j] = ' ';
-			}
-		}
-	}
+    for (i = 0; i < V; i++)
+    {
+        for (j = 0; j < H; j++)
+        {
+            if (i == 0 || i == V - 1)
+            {
+                map[i][j] = '-';
+            }
+            else if (j == 0 || j == H - 1)
+            {
+                map[i][j] = '|';
+            }
+            else
+            {
+                map[i][j] = ' ';
+            }
+        }
+    }
 }
 
-// Introduce every data inIT
-void intro_data(char map[V][H], int *size)
+void setIntroData(char map[V][H], int *size)
 {
+    int i;
 
-	int i;
+    for (i = 1; i < *size; i++)
+    {
+        snake[i].x = snake[i - 1].x - 1;
+        snake[i].y = snake[i - 1].y;
+        snake[i].image = 'X';
+    }
+    snake[0].image = 'O';
 
-	for (i = 1; i < *size; i++)
-	{
+    for (i = 0; i < *size; i++)
+    {
+        map[snake[i].y][snake[i].x] = snake[i].image;
+    }
 
-		snk[i].x = snk[i - 1].x - 1;
-		snk[i].y = snk[i - 1].y;
-
-		snk[i].imagen = 'X'; // body
-	}
-	snk[0].imagen = 'O'; // head
-
-	// Introduce snake into our camp
-
-	for (i = 0; i < *size; i++)
-	{
-
-		map[snk[i].y][snk[i].x] = snk[i].imagen;
-	}
-
-	map[frt.y][frt.x] = 'M'; // fruit
+    map[fruit.y][fruit.x] = 'M';
 }
 
-void loop(char map[V][H], int size)
+void gameLoop(char map[V][H], int size)
 {
+    int isDead = 0;
 
-	int dead;
-
-	dead = 0;
-
-	do
-	{
-		gotoxy(0, 0);
-
-		show(map);
-		input(map, &size, &dead);
-		update(map, size); // automatic
-
-	} while (dead == 0);
+    do
+    {
+        gotoxy(0, 0);
+        showMap(map);
+        getUserInput(map, &size, &isDead);
+        updateGame(map, size);
+    } while (isDead == 0);
 }
 
-void input(char map[V][H], int *size, int *dead)
+void getUserInput(char map[V][H], int *size, int *isDead)
 {
-	int i;
-	char key;
+    int i;
+    char key;
 
-	// Only two ways to die, collision with map or body, every part of the snake is an diferent structure
+    if (snake[0].x == 0 || snake[0].x == H - 1 || snake[0].y == 0 || snake[0].y == V - 1)
+    {
+        *isDead = 1;
+    }
 
-	// CHECK GAME CONDITIONS
+    for (i = 1; i < *size && *isDead == 0; i++)
+    {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+        {
+            *isDead = 1;
+        }
+    }
 
-	if (snk[0].x == 0 || snk[0].x == H - 1 || snk[0].y == 0 || snk[0].y == V - 1)
-	{ // 0 es la cabeza de la serpiente, solo evaluaremos cuando la cabeza choque.
+    if (snake[0].x == fruit.x && snake[0].y == fruit.y)
+    {
+        *size += 1;
+        snake[*size - 1].image = 'X';
+        fruit.x = rand() % (H - 2) + 1;
+        fruit.y = rand() % (V - 2) + 1;
+    }
 
-		*dead = 1;
-	}
+    if (*isDead == 0)
+    {
+        if (kbhit() == 1)
+        {
+            key = getch();
 
-	for (i = 1; i < *size && *dead == 0; i++)
-	{
-
-		if (snk[0].x == snk[i].x && snk[0].y == snk[i].y)
-		{
-			*dead = 1;
-		}
-	}
-
-	// CHECK FRUIT, IF HEAD GET CLOSE, EAT IT!
-
-	if (snk[0].x == frt.x && snk[0].y == frt.y)
-	{
-
-		*size += 1;
-
-		snk[*size - 1].imagen = 'X';
-
-		// Regenerated fruit position in a random position
-
-		frt.x = rand() % (H - 2) + 1;
-		frt.y = rand() % (V - 2) + 1;
-	}
-
-	// IF DEAD IS ZERO WE CAN KEEP GOING
-
-	if (*dead == 0)
-	{
-
-		if (kbhit() == 1)
-		{
-
-			key = getch();
-
-			if (key == 's' && snk[0].movY != -1)
-			{
-
-				snk[0].movX = 0;
-				snk[0].movY = 1;
-			}
-
-			if (key == 'w' && snk[0].movY != 1)
-			{
-
-				snk[0].movX = 0;
-				snk[0].movY = -1;
-			}
-
-			if (key == 'a' && snk[0].movX != 1)
-			{
-
-				snk[0].movX = -1;
-				snk[0].movY = 0;
-			}
-
-			if (key == 'd' && snk[0].movX != -1)
-			{
-
-				snk[0].movX = 1;
-				snk[0].movY = 0;
-			}
-		}
-	}
+            if (key == 's' && snake[0].movY != -1)
+            {
+                snake[0].movX = 0;
+                snake[0].movY = 1;
+            }
+            else if (key == 'w' && snake[0].movY != 1)
+            {
+                snake[0].movX = 0;
+                snake[0].movY = -1;
+            }
+            else if (key == 'a' && snake[0].movX != 1)
+            {
+                snake[0].movX = -1;
+                snake[0].movY = 0;
+            }
+            else if (key == 'd' && snake[0].movX != -1)
+            {
+                snake[0].movX = 1;
+                snake[0].movY = 0;
+            }
+        }
+    }
 }
 
-void update(char map[V][H], int size)
+void updateGame(char map[V][H], int size)
 {
-
-	// Everytime this function is called, the map is overwriting
-
-	intro(map);
-
-	intro_data2(map, size);
+    displayIntro(map);
+    setIntroData2(map, size);
 }
 
-void intro_data2(char map[V][H], int size)
+void setIntroData2(char map[V][H], int size)
 {
+    int i;
 
-	int i;
+    for (i = size - 1; i > 0; i--)
+    {
+        snake[i].x = snake[i - 1].x;
+        snake[i].y = snake[i - 1].y;
+    }
 
-	// Body following the head
+    snake[0].x += snake[0].movX;
+    snake[0].y += snake[0].movY;
 
-	for (i = size - 1; i > 0; i--)
-	{ // 0 is the head. so we going decresing until extremities
+    for (i = 0; i < size; i++)
+    {
+        map[snake[i].y][snake[i].x] = snake[i].image;
+    }
 
-		snk[i].x = snk[i - 1].x;
-		snk[i].y = snk[i - 1].y;
-	}
-
-	snk[0].x += snk[0].movX; // plus movemenrts
-	snk[0].y += snk[0].movY;
-
-	// now introduce the values in the camp
-
-	for (i = 0; i < size; i++)
-	{
-
-		map[snk[i].y][snk[i].x] = snk[i].imagen;
-	}
-
-	map[frt.y][frt.x] = 'M';
+    map[fruit.y][fruit.x] = 'M';
 }
 
 void gotoxy(int x, int y)
-{ // tbh idk what the hell this function is, originally i used another but this works fine (thought) :)
-	COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
